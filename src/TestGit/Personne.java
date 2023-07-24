e7b951d2e1c17c406aa1fb58dbef774c8b7cb7de
samuel.poviefekele <samuel.poviefekele@FR-L4790379.groupinfra.com>
2023-07-24 19:12:54 +0200

    Commit Personne

---
+++ src/TestGit/Personne.java
@@ -0,0 +1,36 @@
+package TestGit;
+
+public class Personne {
+	private String nom;
+	private String prenom; 
+	private Integer age;
+	private String pays;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	
+	
+	
+	
+}
